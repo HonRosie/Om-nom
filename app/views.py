58d9187becee6215d311ad3fa1cf9f283752eb6b
Gloria <HonRosie@Glorias-MacBook-Pro.local>
2015-02-08 14:43:29 -0500

    Todos are now saved to file using pickle

--- app/views.py
+++ app/views.py
@@ -1,15 +1,16 @@
 from flask import render_template
 from app import app
 from flask import request, redirect
+import pickle
 
-todoList = [
-  {'task': 'Buy milk',
-   'comment': 'From Trader Joes'
-  },
-  {'task': 'Bake cookies',
-   'comment': 'Snickerdoodles'
-  }
-]
+# todoList = [
+#   {'task': 'Buy milk',
+#    'comment': 'From Trader Joes'
+#   },
+#   {'task': 'Bake cookies',
+#    'comment': 'Snickerdoodles'
+#   }
+# ]
 
 
 @app.route('/')
@@ -27,10 +28,16 @@ def hello_world():
 @app.route('/todo', methods=['GET','POST'])
 def todos():
 
+  with open('todo', 'rb') as f:
+    todoList = pickle.load(f)
+
   if request.method == 'POST':
     todo = request.form['addTask']
     task = {'task': todo}
     todoList.append(task)
+
+    with open('todo', 'wb') as f:
+      pickle.dump(todoList, f)
 
 
   return render_template('todo.html',
